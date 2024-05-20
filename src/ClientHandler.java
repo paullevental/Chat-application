0d8bb60b81c82b6358f401f8c0b717a37fea7f5d
Paul Levental <paul.levental@gmail.com>
2024-05-19 23:23:10 -0700

    Client handler, added static list of client handlers, implements runnable.

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -1,2 +1,16 @@
-public class ClientHandler {
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ClientHandler implements Runnable {
+
+    public static ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
+    private Socket socket;
+
+
+    @Override
+    public void run() {
+
+    }
+
+
 }
