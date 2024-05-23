9ad07b79a9cc0a9cb4097e6d9659c162bbfc8b07
Paul Levental <paul.levental@gmail.com>
2024-05-23 11:21:18 -0700

    Run method implemented and finished constructor in ClientHandler class

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -16,14 +16,37 @@ public class ClientHandler implements Runnable {
             this.socket = socket;
             this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
             this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.clientUserName = bufferedReader.readLine();
+            System.out.println("SERVER: " + clientUserName + "has entered the chat.");
+            clientHandlers.add(this);
         } catch (IOException e) {
-            e.printStackTrace();
+            closeEverything(socket, bufferedReader, bufferedWriter);
         }
     }
 
 
+
+
     @Override
     public void run() {
+        String clientMessage;
+
+        while (socket.isConnected()) {
+            try {
+                clientMessage = bufferedReader.readLine();
+                transmitMessage(clientMessage);
+            } catch (IOException e) {
+                closeEverything(socket, bufferedReader, bufferedWriter);
+                break;
+            }
+        }
+    }
+
+    public void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter) {
+
+    }
+
+    public  void transmitMessage(String clientMessage) {
 
     }
 
