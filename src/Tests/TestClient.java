7b531dedf0034ab742422fc0c27e390dbc899c01
Paul Levental <paul.levental@gmail.com>
2024-05-29 19:05:23 -0700

    Added Test Client

---
+++ src/Tests/TestClient.java
@@ -0,0 +1,18 @@
+package Tests;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestClient {
+
+
+    @BeforeEach
+    public void runBefore() {
+
+    }
+
+    @Test
+    public void testConstructor() {
+        
+    }
+}
